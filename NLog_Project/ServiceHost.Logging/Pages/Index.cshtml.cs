c8192d4699e83be235ffe5d1e16069a36f44af7f
Maral <mrlmosafercomputer@gmail.com>
2024-04-27 19:25:55 +0330

    Implement: Simple Logging

---
+++ NLog_Project/ServiceHost.Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger; //دسترسی به ای پی آی ثبت لاگ
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //ثبت لاگ
+            //_logger.Log(logLevel:LogLevel.Information,message: "Request Strated...");
+
+            //Extension Method:
+
+            #region Information
+            _logger.LogInformation("Request Ended...");
+            #endregion
+
+            #region Warning
+            string? username = null;
+            if (username is null)
+                _logger.LogWarning("User Not Found...");
+            #endregion
+
+            #region Error
+            try
+            {
+                throw new Exception();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error...");
+            }
+            #endregion
+
+            #region Debug&trace
+            int a = 10, b = 5;
+            int c = a + b;
+            _logger.LogDebug("Sum Of 2 Numbers"); //فرایند
+            _logger.LogTrace($"c = {c}"); //نتیجه فرایند
+            #endregion
+        }
+    }
+}
---
+++ NLog_Project/ServiceHost.Logging/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+#region Logging
+
+builder.Logging.ClearProviders(); //حذف تمامی پرووایردهایی که فعال شده بودن از قبل
+builder.Logging.AddConsole(); //اگر ثبت لاگ کنیم تو کنسول نشون میده
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
