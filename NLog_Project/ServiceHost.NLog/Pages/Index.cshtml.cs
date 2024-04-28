2b31349ada4e5ed667f3e88f6f3fd43918765132
Maral <mrlmosafercomputer@gmail.com>
2024-04-28 19:26:32 +0330

    NLog: Targets

--- NLog_Project/ServiceHost.NLog/Pages/Index.cshtml.cs
+++ NLog_Project/ServiceHost.NLog/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ namespace ServiceHost.NLog.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private static readonly Logger nlog = LogManager.GetCurrentClassLogger(); 
+        private static readonly Logger nlog = LogManager.GetCurrentClassLogger();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,44 +14,46 @@ namespace ServiceHost.NLog.Pages
 
         public void OnGet()
         {
-            #region Information
-            nlog.Info("Request Ended...");
-            #endregion
+            for (int i = 0; i < 10000; i++)
+                nlog.Info($"Test Log For Targets {i}");
+            /*       #region Information
+                   nlog.Info("Request Ended...");
+                   #endregion
 
-            #region Warning
-            string? username = null;
-            if (username is null)
-                nlog.Warn("User Not Found...");
-            #endregion
+                   #region Warning
+                   string? username = null;
+                   if (username is null)
+                       nlog.Warn("User Not Found...");
+                   #endregion
 
-            #region Error
-            try
-            {
-                throw new Exception();
-            }
-            catch (Exception ex)
-            {
-                nlog.Error(ex, "Error Type 1...");
-            }
-            #endregion 
+                   #region Error
+                   try
+                   {
+                       throw new Exception();
+                   }
+                   catch (Exception ex)
+                   {
+                       nlog.Error(ex, "Error Type 1...");
+                   }
+                   #endregion 
 
-            #region Debug&trace
-            int a = 10, b = 5;
-            int c = a + b;
-            nlog.Debug("Sum Of 2 Numbers"); //فرایند
-            nlog.Trace($"c = {c}"); //نتیجه فرایند
-            #endregion
+                   #region Debug&trace
+                   int a = 10, b = 5;
+                   int c = a + b;
+                   nlog.Debug("Sum Of 2 Numbers"); //فرایند
+                   nlog.Trace($"c = {c}"); //نتیجه فرایند
+                   #endregion
 
-            #region Critical
-            try
-            {
-                throw new Exception();
-            }
-            catch (Exception ex)
-            {
-                nlog.Fatal(ex, "Example: Can not connect To DataBase...");
-            }
-            #endregion
+                   #region Critical
+                   try
+                   {
+                       throw new Exception();
+                   }
+                   catch (Exception ex)
+                   {
+                       nlog.Fatal(ex, "Example: Can not connect To DataBase...");
+                   }
+                   #endregion*/
         }
     }
 }
